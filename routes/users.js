558b2f2f5e9dd749eec6f3012b86e95ee27cfaa8
Joseph Henshaw <furiousjoe16@gmail.com>
2019-11-16 18:23:36 +0100

    Add vendor registration and login routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 module.exports = function (io) {
   const {INTERNAL_SERVER_ERROR, OK} = require("http-status-codes");
-  const {Router} = require('express');
+  const Router = require('express');
+  const Vendor = require('../controllers/vendorController');
   const User = require('../controllers/usersController.js')(io);
-
+  const {CREATED} = require("http-status-codes");
 
   const router = Router();
   const error = 'Oops an error occurred';
@@ -10,6 +11,7 @@ module.exports = function (io) {
   router.post('/signup', async function (req, res) {
     try {
       const token = await User.signUp(req);
+      // FIXME: Return an error and in the route, check for an error then return result from function call;
 
       res.status(OK).json({
         token,
@@ -45,6 +47,34 @@ module.exports = function (io) {
       res.status(OK).json({
         status: 'Registered'
       })
+    } catch (e) {
+      res.status(INTERNAL_SERVER_ERROR).json({
+        error,
+      })
+    }
+  });
+
+  router.post('/vendorlogin', async (req, res) => {
+    try {
+      await User.vendorLogin(req);
+      res.status(OK).json(
+          {success: 'Logged In'}
+      )
+    } catch (e) {
+      res.status(INTERNAL_SERVER_ERROR).json({
+        error,
+      })
+    }
+  });
+
+  router.post('/registervendor', async (req, res) => {
+    try {
+      const token = await Vendor.signUp(req);
+
+      res.status(CREATED).json({
+        token,
+      });
+
     } catch (e) {
       res.status(INTERNAL_SERVER_ERROR).json({
         error,
