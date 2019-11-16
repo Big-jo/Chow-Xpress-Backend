2391db60e6a888210a7cf5da30e63daff640030d
Joseph Henshaw <furiousjoe16@gmail.com>
2019-11-16 18:24:06 +0100

    Add vendor addItem method

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,15 +1,20 @@
 const VendorModel = require('../models/vendorModel');
 const bcrypt = require('bcrypt');
-const {CreateToken} = require("../lib/jsonWebToken");
+const CreateToken = require("../lib/jsonWebToken");
 
+/**
+ *
+ * @returns {*}
+ * Vendor Controller
+ */
 
-const Vendor = async() => {
-    this.signUp = async (req) => {
-        const Vendor = await VendorModel.findOne({"email": req.body.email});
-        if (Vendor) {
-            return 'Oops this User already'
-        } else {
-            try {
+Vendor = () => {
+    this.signUp = async function (req) {
+        try {
+            const Vendor = await VendorModel.findOne({"Name": req.body.Name});
+            if (Vendor !== null) {
+                return 'Oops this User already'
+            } else {
                 const Vendor = {
                     Name: req.body.Name,
                     Password: req.body.Password,
@@ -19,39 +24,45 @@ const Vendor = async() => {
                 const vendor = await new VendorModel(Vendor);
                 const saltRounds = 10;
                 vendor.Password = await bcrypt.hash(vendor.Password, saltRounds);
-                vendor.save();
-                const token = CreateToken(vendor);
+                await vendor.save();
 
-                return {token};
-            } catch (e) {
-                throw e;
+                return await CreateToken(vendor);
             }
+        } catch (e) {
+            throw new Error(e);
         }
     };
 
-    this.login = async (req) => {
-            try {
-                const Vendor = await VendorModel.findOne({email: req.body.email});
-
-                if (Vendor) {
-                    const vendorPassword = Vendor.Password;
-                    const requestPassword = req.body.Password;
-                    const samePassword = await bcrypt.compare(requestPassword, vendorPassword);
-
-                    if(samePassword) {
-                        const token = await CreateToken(Vendor);
-                        return {token}
-                    } else {
-                        return 'Sorry, this does not match what we have';
-                    }
-                }
-            } catch (e) {
-                throw e;
-            }
-        };
+    this.addItem = async (req) => {
+        try {
+            await VendorModel.findOneAndUpdate({"Name": req.body.name}, {$push: {Items: req.body.item}}).exec();
+        } catch (e) {
+            throw new Error(e);
+        }
+    };
+    // this.login = async (req) => {
+    //         try {
+    //             const Vendor = await VendorModel.findOne({email: req.body.email});
+    //
+    //             if (Vendor) {
+    //                 const vendorPassword = Vendor.Password;
+    //                 const requestPassword = req.body.Password;
+    //                 const samePassword = await bcrypt.compare(requestPassword, vendorPassword);
+    //
+    //                 if(samePassword) {
+    //                     const token = await CreateToken(Vendor);
+    //                     return {token}
+    //                 } else {
+    //                     return 'Sorry, this does not match what we have';
+    //                 }
+    //             }
+    //         } catch (e) {
+    //             throw new Error (e);
+    //         }
+    //     };
 
     return this;
 };
 
-exports.vendor = Vendor();
+module.exports = Vendor();
 // FIXME: use standard node.js imports throughout all modules.
